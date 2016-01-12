53a9f25ae532133592a8c190938df2ff8b25bbce
imranariffin <imranariffin@yahoo.com>
2016-01-12 14:31:54 -0500

    CORS success
    Former-commit-id: febc6d047840dbde0d54dbaa7258acc0d7640060

--- app.py
+++ app.py
@@ -7,8 +7,22 @@ from sys import argv
 
 import bottle
 from bottle import default_app, request, route, response, get
+from pymongo import MongoClient
 
 bottle.debug(True)
+
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
 
 @get('/')
 def index():
@@ -29,5 +43,20 @@ def index():
         ret += '%s=%s\n' % (k, v)
 
     return ret
+
+@route
+def yo():
+    return "yo"
+@route('/login', method=["OPTIONS", "POST"])
+@enable_cors
+def post_login(**user):
+    response.headers['Content-type'] = 'application/json'
+    import json
+    res = json.loads(request.forms.keys()[0])
+    print res
+    print dir(request.POST)
+    res['loginSuccess'] = "false"
+    return json.dumps(res)
+
 
 bottle.run(host='0.0.0.0', port=argv[1])
